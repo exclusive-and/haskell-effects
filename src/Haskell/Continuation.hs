87c87b7641032f91dd28175589aaed5c21b8ed2b
exclusive-and <exclusiveandgate@gmail.com>
2023-12-21 16:08:41 -0500

    Hopefully final documentation

--- src/Haskell/Continuation.hs
+++ src/Haskell/Continuation.hs
@@ -1,13 +1,13 @@
 
--- | Module     :   Haskell.Continuation
+-- | Module         :   Haskell.Continuation
 --
---   Copyright  :   (c) 2023, Simon Lovell Bart
---   License    :   BSD3 (see the file LICENSE)
---   Maintainer :   Simon Lovell Bart
---                  <exclusiveandgate@gmail.com>
+--   Copyright      :   (c) 2023, Simon Lovell Bart
+--   License        :   BSD3 (see the file LICENSE)
+--   Maintainer     :   Simon Lovell Bart
+--                      <exclusiveandgate@gmail.com>
 --
--- A first-class notion of the computing machine: this module defines
--- an architecture-agnostic representation of code evaluation.
+-- A first-class notion of the computing machine: this module defines some
+-- combinators that can directly control code execution.
 
 {-# LANGUAGE MagicHash, UnboxedTuples #-}
 
@@ -17,24 +17,26 @@ import GHC.Exts (PromptTag#, newPromptTag#, prompt#, control0#)
 import GHC.IO (IO (..))
 
 
--- |
-data ContId a = ContId (PromptTag# a)
+-- | 'ContTag's are how the control combinators refer to parts of the code.
+data ContTag a = ContTag (PromptTag# a)
 
--- |
-newContinuation :: IO (ContId a)
+-- | Create a new 'ContTag'.
+newContinuation :: IO (ContTag a)
 newContinuation = IO $ \s0 -> case newPromptTag# s0 of
-    (# s1, tag #) -> (# s1, ContId tag #)
+    (# s1, tag #) -> (# s1, ContTag tag #)
 
--- |
-delimit :: ContId a -> IO a -> IO a
-delimit (ContId tag) (IO m) = IO (prompt# tag m)
+-- | Delimits a section of code that 'control0' can terminate.
+delimit :: ContTag a -> IO a -> IO a
+delimit (ContTag tag) (IO m) = IO (prompt# tag m)
 
 -- | The type of functions in continuation-passing style (CPS).
 type CPS r a = (IO a -> IO r) -> IO r
 
--- |
-control0 :: ContId r -> CPS r a -> IO a
-control0 (ContId tag) run = IO (control0# tag continue)
+-- | A deviant kind of function that can terminate the computation of any
+-- code containing it with an artificial return value. It also creates a
+-- special closure value which, when it is evaluated, causes the machine to
+-- resume the terminated computation.
+control0 :: ContTag r -> CPS r a -> IO a
+control0 (ContTag tag) run = IO (control0# tag continue)
     where
     continue k = case run (\(IO a) -> IO (k a)) of IO r -> r
-
